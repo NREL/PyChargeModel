fea83988c71dc286380586393079d0fea91555e0
mjun70 <78111354+mjun70@users.noreply.github.com>
2022-03-02 09:26:30 -0700

    Update evse_class.py

--- evse_class.py
+++ evse_class.py
@@ -3,31 +3,36 @@ class EVSE_class():
         self.efficiency = efficiency
         self.Prated_kW  = Prated_kW
         self.evse_id   = evse_id
-        
-        self.ev.voltage = 0.0
-        self.ev.power   = 0.0
-        self.ev.soc     = 0.0
-        self.ev.plugged = False
-        
-        self.server.setpoint = 0.0
+
+        self.ev_voltage = 0.0
+        self.ev_power   = 0.0
+        self.ev_soc     = 0.0
+        self.ev_plugged = False
+
+        self.server_setpoint = 0.0
+
        
     def receive_from_ev(self, Vbatt, Pbatt_kW, soc, plugged):
-        self.ev.voltage = Vbatt
-        self.ev.power   = Pbatt_kW
-        self.ev.soc     = soc
-        self.ev.plugged = plugged
-        
+        self.ev_voltage = Vbatt
+        self.ev_power   = Pbatt_kW
+        self.ev_soc     = soc
+        self.ev_plugged = plugged
+
+
     def send_to_ev(self):
-        Pmax = min(self.server.setpoint, self.Prated_kW)*self.efficiency
+        Pmax = min(self.server_setpoint, self.Prated_kW)*self.efficiency
         
         return Pmax
-        
+
+
     def receive_from_server(self, setpoint_kW):
-        self.server.setpoint = setpoint_kW
-        
+        self.server_setpoint = setpoint_kW
+
+
     def send_to_server(self):
-        Vbatt    = self.ev.voltage
-        Pbatt_kW = self.ev.power
-        soc      = self.ev.soc
+        Vbatt    = self.ev_voltage
+        Pbatt_kW = self.ev_power
+        soc      = self.ev_soc
         
         return [Vbatt, Pbatt_kW, soc]
+
